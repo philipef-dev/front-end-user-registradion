a03b36d62028a92565ffa44317341151764c9fb4
PhilipeF <philipefdev@gmail.com>
2025-03-05 19:50:56 -0300

    Adicionando tabela para lista de usuários

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import api from "./services/api";
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+}
+
 export default function Home() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    api.get("/users")
+      .then((response) => setUsers(response.data))
+      .catch((err) => {
+        console.error(`ops! ocorreu um erro`, err)
+      })
+  }, [])
+
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
-      <form className="bg-white p-4 rounded shadow-md w-1/3">
+    <div className="flex flex-col justify-center items-center min-h-screen  bg-gray-100">
+      <form className="bg-white p-4 rounded shadow-md w-1/3 mb-4">
         <h1 className="text-2xl text-center mb-4 font-semibold">
           Cadastro de Usuários
         </h1>
@@ -33,14 +55,14 @@ export default function Home() {
         </div>
         <div className="mb-2">
           <label
-            htmlFor="name"
+            htmlFor="age"
             className=" block font-medium text-sm text-gray-700"
           >
             Idade:
           </label>
           <input
-            type="text"
-            id="idade"
+            type="number"
+            id="age"
             className="border border-gray-300 p-2 w-full rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-300 required"
           />
         </div>
@@ -49,10 +71,29 @@ export default function Home() {
             type="submit"
             className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 transition-all rounded-md"
           >
-            Cadastrar
+            Salvar
           </button>
         </div>
       </form>
+      <h1 className="text-lg font-bold">Lista de usuários</h1>
+      <table className="bg-white w-1/3 mt-4 p-4 border border-gray-200 rounded-b-lg shadow-md">
+        <thead>
+          <tr className="text-left bg-gray-100 text-gray-700">
+            <th className="px-4 py-2 border-b text-center">Nome completo</th>
+            <th className="px-4 py-2 border-b text-center">E-mail</th>
+            <th className="px-4 py-2 border-b text-center">Idade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id} className="hover:bg-gray-50">
+              <td className="px-4 py-2 border-b">{user.name} </td>
+              <td className="px-4 py-2 border-b">{user.email} </td>
+              <td className="px-4 py-2 border-b"> {user.age} </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
