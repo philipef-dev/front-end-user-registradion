07ed25d7f75ef932de558c1fc4f4c1c884beb686
PhilipeF <philipefdev@gmail.com>
2025-03-05 20:24:05 -0300

    Componentizando o formulario

---
+++ src/app/components/form/Form.tsx
@@ -0,0 +1,62 @@
+const Form = () => {
+    function handleSubmit (e:React.FormEvent) {
+        e.preventDefault();
+    }    
+    return (
+        <>
+            <form onSubmit={handleSubmit} className="bg-white p-4 rounded shadow-md w-1/3 mb-4" >
+                <h1 className="text-2xl text-center mb-4 font-semibold">
+                    Cadastro de Usuários
+                </h1>
+                <div className="mb-2">
+                    <label
+                        htmlFor="name"
+                        className=" block font-medium text-sm text-gray-700"
+                    >
+                        Nome:
+                    </label>
+                    <input
+                        type="text"
+                        id="name"
+                        className="border border-gray-300 p-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300 required"
+                    />
+                </div>
+                <div className="mb-2">
+                    <label
+                        htmlFor="email"
+                        className=" block font-medium text-sm text-gray-700"
+                    >
+                        E-mail:
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        className="border border-gray-300 p-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300 required"
+                    />
+                </div>
+                <div className="mb-2">
+                    <label
+                        htmlFor="age"
+                        className=" block font-medium text-sm text-gray-700"
+                    >
+                        Idade:
+                    </label>
+                    <input
+                        type="number"
+                        id="age"
+                        className="border border-gray-300 p-2 w-full rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-300 required"
+                    />
+                </div>
+                <div className="mt-4 text-center">
+                    <button
+                        type="submit"
+                        className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 transition-all rounded-md"
+                    >
+                        Salvar
+                    </button>
+                </div>
+            </form >
+        </>
+)}
+
+export default Form
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 
 import { useEffect, useState } from "react"
 import api from "./services/api";
+import { CiEdit } from 'react-icons/ci';
+import { MdDeleteForever } from 'react-icons/md';
+import Form from "./components/form/Form";
 
 type User = {
   id: number;
@@ -19,62 +22,11 @@ export default function Home() {
       .catch((err) => {
         console.error(`ops! ocorreu um erro`, err)
       })
-  }, [])
+  }, [])  
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen  bg-gray-100">
-      <form className="bg-white p-4 rounded shadow-md w-1/3 mb-4">
-        <h1 className="text-2xl text-center mb-4 font-semibold">
-          Cadastro de Usuários
-        </h1>
-        <div className="mb-2">
-          <label
-            htmlFor="name"
-            className=" block font-medium text-sm text-gray-700"
-          >
-            Nome:
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="border border-gray-300 p-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300 required"
-          />
-        </div>
-        <div className="mb-2">
-          <label
-            htmlFor="email"
-            className=" block font-medium text-sm text-gray-700"
-          >
-            E-mail:
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="border border-gray-300 p-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300 required"
-          />
-        </div>
-        <div className="mb-2">
-          <label
-            htmlFor="age"
-            className=" block font-medium text-sm text-gray-700"
-          >
-            Idade:
-          </label>
-          <input
-            type="number"
-            id="age"
-            className="border border-gray-300 p-2 w-full rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-300 required"
-          />
-        </div>
-        <div className="mt-4 text-center">
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 transition-all rounded-md"
-          >
-            Salvar
-          </button>
-        </div>
-      </form>
+      <Form/>
       <h1 className="text-lg font-bold">Lista de usuários</h1>
       <table className="bg-white w-1/3 mt-4 p-4 border border-gray-200 rounded-b-lg shadow-md">
         <thead>
@@ -82,6 +34,7 @@ export default function Home() {
             <th className="px-4 py-2 border-b text-center">Nome completo</th>
             <th className="px-4 py-2 border-b text-center">E-mail</th>
             <th className="px-4 py-2 border-b text-center">Idade</th>
+            <th className="px-4 py-2 border-b text-center">Ações</th>
           </tr>
         </thead>
         <tbody>
@@ -90,6 +43,12 @@ export default function Home() {
               <td className="px-4 py-2 border-b">{user.name} </td>
               <td className="px-4 py-2 border-b">{user.email} </td>
               <td className="px-4 py-2 border-b"> {user.age} </td>
+              <td className="px-4 py-2 border-b">
+                <div className="flex space-x-2">
+                  <CiEdit className="cursor-pointer" /> 
+                  <MdDeleteForever className="cursor-pointer" />
+                </div>
+              </td>
             </tr>
           ))}
         </tbody>
